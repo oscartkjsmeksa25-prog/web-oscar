33120618f1114495fdb545d608b2ce5d73862069
oscartkjsmeksa25-prog <oscar.tkjsmeksa25@gmail.com>
2026-08-05 21:17:30 +0700

    Add files via upload

---
+++ ganjil.genap.py
@@ -0,0 +1,6 @@
+angka = int(input("Masukkan angka: "))
+
+if angka % 2 == 0:
+    print(f"{angka} adalah bilangan genap.")
+else:
+    print(f"{angka} adalah bilangan ganjil.")
---
+++ if_else.py
@@ -0,0 +1,6 @@
+nilai = int(input("Masukkan nilai: "))
+
+if nilai >= 75:
+    print("Selamat, Anda LULUS.")
+else:
+    print("Maaf, Anda TIDAK LULUS.")
